13bee8511a292e4bb2f3dd88c8210d4d3b2346dd
Mohammed <hassan.nazamul05@gmail.com>
2024-05-24 18:04:36 -0400

    connected to the data base and inserted the value

--- src/main/resources/static/test.sql
+++ src/main/resources/static/test.sql
@@ -11,3 +11,16 @@ VALUES ('cat1', 'THIS IS CAKE 3');
 
 INSERT INTO PRODUCT (category,description)
 VALUES ('cat1', 'THIS IS CAKE 4');
+
+-- For Free SQL
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 1','10', 'cake 1\r\n');
+
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 2','20', 'cake 2\r\n');
+
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 3','30', 'cake 3\r\n');
+
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 4','40', 'cake 4\r\n');
+
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 5','50', 'cake 5\r\n');
+
+INSERT INTO `product` (`category`, `description`, `price`, `product_name`) VALUES ( 'cat1', 'product 6','60', 'cake 6\r\n');
